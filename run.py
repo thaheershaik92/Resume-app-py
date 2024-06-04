700f7560374bfc2fb68c1359f5eeaba8fa601782
Shaik Thaheer <128355075+thaheershaik92@users.noreply.github.com>
2024-06-04 16:29:15 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,5 +2,10 @@ from app import create_app
 
 app = create_app()
 
+# Pass the required route to the decorator
+@app.route("/")
+def index(): 
+    return "Homepage for Website"
+    
 if __name__ == '__main__':
     app.run(debug=True)
